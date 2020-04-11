51f295fdc79fae34e9adccd8379133d299ede34c
fantastyJ <fantastyj@fantastyJdeMacBook-Pro.local>
2020-04-11 17:08:50 +0800

    优化完成

--- selenium_12306_ticket_exact_mode_new.py
+++ selenium_12306_ticket_exact_mode_new.py
@@ -6,15 +6,18 @@ import json
 import time
 import datetime
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException,NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
-import asyncio
+from chaojiying import chaojiying  # 第三方工具超级鹰的调用代码
+from PIL import Image  # 对于后续对验证码图片需要进行保存读取的操作
+import os
 import re
+import getpass
 
-code_login = False
+code_login = False #True扫码  False账号密码
 time_out = 60
 poll_frequency = 0.1
 requery = True
@@ -32,19 +35,6 @@ seat_no = {
     '无座':10
 }
 
-seat_type_index = {
-    '商务座': '9',
-    '一等座': 'M',
-    '二等座': '0',
-    '高级软卧': '4',
-    '软卧': '5',
-    '动卧': '6',
-    '硬卧': '3',
-    '软座': '8',
-    '硬座': '1',
-    '无座': '10'
-}
-
 def print_t(*content):
     print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())),*content)
 
@@ -58,6 +48,30 @@ def get_citylist_from_12306():
         dict_city[l_list[1]] = l_list[2]
 
     return dict_city
+
+# 下载所有的车次数据  保存为 train_list.txt文件
+def get_train_list():
+    requests.adapters.DEFAULT_RETRIES = 5
+    response = requests.get('https://kyfw.12306.cn/otn/resources/js/query//train_list.js?scriptVersion=1.0', stream=True, verify=False)
+    status = response.status_code
+    if status == 200:
+        with open('./config/train_list.txt', 'wb') as of:
+            for chunk in response.iter_content(chunk_size=102400):
+                if chunk:
+                    of.write(chunk)
+
+def get_train_no(train_code):
+    c = list(train_code)[0]
+    with open('./config/train_list.txt', 'rb') as of:
+        text = of.readline()
+        tt = text.decode("utf-8")
+        ss = tt.replace('var train_list =','').replace("},{", "},\n{")
+
+        d = json.loads(ss)
+        date = '2019-07-16'
+        for k in d[date][c]:
+            if k['station_train_code'].find(train_code+'(')>-1:
+                return k['train_no']
 
 def wait_loading_or_exit(driver,xpath,msg='等待加载完成'):
     try:
@@ -73,7 +87,13 @@ def wait_loading_or_exit(driver,xpath,msg='等待加载完成'):
         exit(1)
 
 def click_query_ticket(driver):
-    driver.find_element_by_xpath('//a[@id="query_ticket"]').click()
+    try:
+        driver.find_element_by_xpath('//a[@id="query_ticket"]').click()
+    except Exception as e:
+        if not check_query_ticket_success(driver):
+            time.sleep(1)
+            click_query_ticket(driver)
+
 
     print_t('点击查询按钮并且等待恢复可点击状态(等待查询完成)')
     try:
@@ -112,64 +132,147 @@ def query_ticket_or_requery(driver):
     while not check_query_ticket_success(driver):
         click_query_ticket(driver)
 
-    loop_await([async_await_parse_tr(tr) for tr in driver.find_elements_by_xpath('//tbody[@id="queryLeftTable"]/tr')])
+    TRAIN_NO_TAIL=['00','01','02','03','04','05','06','07','08','09','0A','0B','0C','0D','0F']
+    findTrainNo = False
+    for tail in TRAIN_NO_TAIL:
+        if findTrainNo:#已经找到了，退出循环
+            break
+        trainNo = train_no[:-2]+tail
+        try:
+            tr = driver.find_element_by_id("ticket_"+trainNo)
+            print_t("找到车次: " + trainNo)
+            row_list =[td.text for td in tr.find_elements_by_xpath('./td')]
+            print_t(row_list)
+            if len(row_list) > 0:
+                train_code = row_list[0].split('\n')[0]
+                print_t(train_code)
+                if train_code == ticket_12306_config_dict['train_code']:
+                    for seat in ticket_12306_config_dict['train_seat']:
+                        print_t(seat)
+                        if has_seat(row_list, seat_no[seat]):
+                            requery = False
+
+                            tr.find_element_by_xpath('./td/a').click()
+                            print_t("查票成功跳转到购买页")
+                            findTrainNo = True
+                            break
+        except Exception as e:
+            if isinstance(e,NoSuchElementException):
+                print_t("没有找到 "+trainNo)
+                # print_t("没有找到该车次，可能该车次已经停售，系统将继续尝试，你可以Ctrl C退出选择其他车次")
 
     if requery:
         print_t('没有票，将再次发起查询')
         query_ticket_or_requery(driver)
 
-async def async_await_parse_tr(tr):
-    global requery
-    try:
-        eles = await async_tr_td_list(tr)
-        if eles:
-            row_list = [td.text for td in eles]
-            print(row_list)
-            if len(row_list) > 0:
-                train_code = row_list[0].split('\n')[0]
-                print(train_code)
-                for priority in ticket_12306_config_dict['priority_train']:
-                    if train_code == priority['train_code']:
-                        for seat in priority['train_seat']:
-                            print(seat)
-                            if has_seat(row_list, seat_no[seat]):
-                                requery = False
-
-                                tr.find_element_by_xpath('./td/a').click()
-                                print_t("查票成功跳转到购买页")
-                                break
-    except Exception as e:
-        pass
-
 def has_seat(row_list,no):
-    return row_list[no] == '有' or isinstance(row_list[no],int)
+    return row_list[no] == '有' or isinstance(int(row_list[no]),int)
 
-async def async_tr_td_list(tr):
-    return tr.find_elements_by_xpath('./td')
-
-def loop_await(tasks):
-    global requery
-    try:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        loop.run_until_complete(asyncio.wait(tasks))
-        loop.close()
-    except Exception as e:
-        if not requery:
-            print_t('已经跳转到购买页，查找失败')
-    finally:
-        if loop:
-            loop.close()
 
 def get_right_train(buy_train_code,ticket_12306_config_dict):
     for priority in ticket_12306_config_dict['priority_train']:
         if priority['train_code'] == buy_train_code :
             return priority
 
+# 图形验证码
+def code_distinguish(browser):
+    # 拿到验证码图片的节点定位
+    code_img = browser.find_element_by_css_selector('#J-loginImg')
+    # 2.1这里的location是获取这张图片的左上角的左边，返回的是字典封装的x,y的值.基于整个网页返回给你的值
+    loca = code_img.location  # x,y{'x': 856, 'y': 284}
+    # 1.所以，一般还需要拿到这张图片的尺寸，即长和宽
+    size = code_img.size  # {'height': 188, 'width': 300}
+    # 2.2接下来，就是需要做截屏。
+    # 1.需要定位左上角和右下角2个点的左边，即共4个.保存在元祖中
+    code_position = (int(loca['x']), int(loca['y']), int(loca['x'] + size['width']), int(loca['y'] + size['height']))
+    # 2.调用截图功能，将图片截取下去。这个功能在无头浏览器中应用的比较多，比如你想验证无头浏览器是否访问到了你想要的页面，就可以使用截图来测试观看
+    # 3.截取当前浏览器打开的这张页面的整个图像（记住，是整个浏览器打开的页面的图像）
+    browser.save_screenshot('./pic/aa.png')
+    # 4.用Image包打开这张截图
+    i = Image.open('pic/aa.png')
+    # 5.给要截取的验证码图片取名
+    code_img_name = './pic/code.png'
+    # 6.调用crop函数，在这大的图片里面通过2个点对其内部的内容进行截图
+    frame = i.crop(code_position)
+    # 7.将截取下来的图片存到目录中，并取名
+    frame.save(code_img_name)
+
+    # 2.3调用超级鹰进行识别，拿到返回的结果
+    im = open('pic/code.png', 'rb').read()
+    # {'err_no': 0, 'err_str': 'OK', 'pic_id': '3095313233818200001', 'pic_str': '25,143|260,134', 'md5': '29c583678bdf3eb09ece5726fcfaa285'}
+    result = chaojiying.PostPic(im, 9004)
+    print(result)
+    coords = result['pic_str']
+    chaojiying.ReportError(result['pic_id'])
+    # 1.解析超级鹰返回的数据
+    # 先声明一个数组用来存储所有的图片坐标,想要的是这种结果[[25,143],[260,134]]
+    all_list = []
+    if '|' in coords:
+        list1 = coords.split('|')  # -->['25,143', '260,134']
+        count_list = len(list1)
+        for i in range(count_list):
+            xy = []
+            x = int(list1[i].split(',')[0])
+            y = int(list1[i].split(',')[1])
+            xy.append(x)
+            xy.append(y)
+            all_list.append(xy)
+        # 针对的是只有一组图片符合的数据['25,143']
+    else:
+        xy = []
+        x = int(coords.split(',')[0])
+        y = int(coords.split(',')[1])
+        xy.append(x)
+        xy.append(y)
+        all_list.append(xy)
+    # 2.4拿到返回结果后，需要调用动作链，以此点击选中的图片坐标.
+    action = ActionChains(browser)
+    for i in all_list:
+        x = i[0]
+        y = i[1]
+        # 2.4.1:这样使用move_to_element_with_offset。表示选中一个区域再进行移动。因为返回的坐标数值是以发送的图片作为参照，而我们需要进行点击是要在整张页面中。为了解决这个问题，需要把范围先切换到页面中的图片上
+        print('鼠标移动到图片的相对坐标，x:',x,', y:',y)
+        ActionChains(browser).move_to_element_with_offset(code_img, x, y).click().perform()
+    time.sleep(1)
+
+def login(driver):
+    username = input("Please input your username:")
+    password = getpass.getpass("Please input your password:")
+
+    if not username or not password:
+        print_t("用户名或密码为空")
+        exit(1)
+
+    # 点击进行账号密码登陆
+    driver.find_element_by_css_selector('body > div.login-panel > div.login-box > ul > li.login-hd-account > a').click()
+    time.sleep(1)
+    # 进行图片选择
+    code_distinguish(driver)
+
+    print_t('开始自动填充用户名密码')
+
+    driver.find_element_by_xpath('//input[@id="J-userName"]').clear()
+    # driver.find_element_by_xpath('//input[@id="J-userName"]').send_keys(ticket_12306_config_dict['username'])  # 填充用户名
+    driver.find_element_by_xpath('//input[@id="J-userName"]').send_keys(username)  # 填充用户名
+
+    driver.find_element_by_xpath('//input[@id="J-password"]').clear()
+    # driver.find_element_by_xpath('//input[@id="J-password"]').send_keys(ticket_12306_config_dict['password'])  # 填充用户名
+    driver.find_element_by_xpath('//input[@id="J-password"]').send_keys(password)  # 填充用户名
+
+    time.sleep(1)
+    driver.find_element_by_css_selector('#J-login').click()
+    try:
+        WebDriverWait(driver, 3, poll_frequency).until(
+            lambda x: x.find_element_by_xpath('//div[@class="modal-ft"]').is_displayed())
+    except Exception as e:
+        print('密码错误或验证码有误，重试~~~~')
+        login(driver)
+
+
 if __name__ == '__main__':
     print_t('开始读取配置文件')
     try:
-        with open('./config/ticket_12306_config.json','r') as f:
+        with open('./config/ticket_12306_exact_mode_config.json','r') as f:
             ticket_12306_config = f.read()
     except FileNotFoundError as e:
         print_t('没有找到配置文件，系统已退出，请先下载配置文件ticket_12306_config.json到当前运行环境目录下的config目录')
@@ -207,44 +310,39 @@ if __name__ == '__main__':
         print_t('请填写正确的出发时间信息')
         exit(1)
 
+    if not os.path.exists("./config/train_list.txt"):
+        print_t('开始下载当前12306全部车次信息，50M左右，下载时间比较长，请耐心等待')
+        get_train_list()
+        print_t('下载当前12306全部车次信息，50M左右，下载完成')
+
+    train_no = get_train_no(ticket_12306_config_dict['train_code'])
+    if not train_no:
+        print_t('你当前要购买的车次不存在，无法购买，系统将退出')
+
+
     print_t('系统配置读取完成')
     print('   您将为',ticket_12306_config_dict['passenger_list'],'购买在',\
             ticket_12306_config_dict['travel_date'],'由',ticket_12306_config_dict['from_station_text'],\
             '开往', ticket_12306_config_dict['to_station_text'],'的列车')
-    print('系统将依次优先选择')
-    for t in ticket_12306_config_dict['priority_train']:
-        print('  ',t['train_code'],'列车的',t['train_seat'],'席位')
+    print('   系统将选择',ticket_12306_config_dict['train_code'],'列车的',ticket_12306_config_dict['train_seat'],'席位')
+
 
     print_t('准备完成即将开始购票')
 
-    driver = webdriver.Chrome('./chromedriver')
-
+    driver = webdriver.Chrome('/usr/local/Caskroom/chromedriver/80.0.3987.106/chromedriver')
+    driver.maximize_window()
     print_t('开始进入登录页面')
     driver.get('https://kyfw.12306.cn/otn/resources/login.html')
 
     wait_loading_or_exit(driver,'//div[@class="login-code"]/div[@class="login-code-con"]/div[@class="login-code-main"]/div[@class="code-pic"]/img[@id="J-qrImg"]','等待扫码登录登录码加载完成')
 
-    print_t('系统目前启用账号密码登录，建议为了提高速度启用扫码登录，启用方式为修改修改 code_login 为 False')
+    print_t('系统目前启用扫码登录，请打开手机12306客户端完成扫码登录，启用账户密码的方式为修改为 code_login 为 False')
     if not code_login:
-        driver.find_element_by_xpath(
-            '//div[@class="login-box"]/ul[@class="login-hd"]/li[@class="login-hd-account"]/a').click()
+        login(driver)
 
-        print_t('开始自动填充用户名密码')
-
-        driver.find_element_by_xpath('//input[@id="J-userName"]').clear()
-        driver.find_element_by_xpath('//input[@id="J-userName"]').send_keys(ticket_12306_config_dict['username'])  # 填充用户名
-
-        driver.find_element_by_xpath('//input[@id="J-password"]').clear()
-        driver.find_element_by_xpath('//input[@id="J-password"]').send_keys(ticket_12306_config_dict['password'])  # 填充用户名
-
-        wait_loading_or_exit(driver,
-                             '//div[@class="touclick-wrapper lgcode-2018"]/div[@id="J-loginImgArea"]/img[@id="J-loginImg"]',
-                             '等待图形验证码加载完成')
-
-    input_no = input("请检查网页是否有图形验证码需要点击，需要就请在在网页点击验证码图片，点击完成后，控制台输入1回车；如果无需点击验证码，则直接回车")
-    if input_no and '' != input_no and int(input_no) == 1:
-        driver.find_element_by_xpath('//div[@class="login-btn"]/a[@class="btn btn-primary form-block"]').click()
-
+    # 自动点击提示框的 "确认"按钮
+    wait_loading_or_exit(driver,'//div[@class="modal-ft"]', '等待提示框的出现')
+    driver.find_element_by_css_selector('.ok').click()
 
     wait_loading_or_exit(driver, '//li[@id="J-header-logout"]', '等待登录完成')
 
@@ -300,14 +398,13 @@ if __name__ == '__main__':
 
     buy_train_code = driver.find_element_by_xpath('//p[@id="ticket_tit_id"]/strong[@class="ml5"]').text
 
-    for i, passerger in enumerate(ticket_12306_config_dict['passenger_list']):
-        selector = Select(driver.find_element_by_id("seatType_" + str(i + 1)))
-        select_seat_list = [o.text.replace(' ', '').replace('\n', '') for o in selector.options]
-        print_t('自动选择座位类型，目前可选择的座位类型有', select_seat_list)
-        right_train = get_right_train(buy_train_code,ticket_12306_config_dict)
+    for i,passerger in enumerate(ticket_12306_config_dict['passenger_list']):
+        selector = Select(driver.find_element_by_id("seatType_"+str(i+1)))
+        select_seat_list = [o.text.replace(' ','').replace('\n','')  for o in selector.options]
+        print_t('自动选择座位类型，目前可选择的座位类型有',select_seat_list)
 
         set_seat_success = False
-        for train_seat in right_train['train_seat']:
+        for train_seat in ticket_12306_config_dict['train_seat']:
             for i,select_seat in enumerate(select_seat_list):
                 if select_seat.find(train_seat)>-1 and not set_seat_success:
                     print_t('为',passerger,'自动选择座位类型，目前为你成功选择座位类型为', train_seat)
@@ -315,10 +412,10 @@ if __name__ == '__main__':
                     set_seat_success = True
                     break
 
-
+    time.sleep(1)
     print_t('自动点击提交购票按钮')
     driver.find_element_by_id("submitOrder_id").click()
-
+    time.sleep(1)
     print_t('等待确认购买按钮加载完成')
     try:
         WebDriverWait(driver, 10, 0.1).until(
@@ -330,11 +427,11 @@ if __name__ == '__main__':
             print(e)
         driver.close()
         exit(1)
-
+    time.sleep(2)
     print_t('自动点击确认购买按钮')
-    driver.find_element_by_id("qr_submit_id").click()
-
+    # driver.find_element_by_id("qr_submit_id").click()
     input("完成购买！请在30分钟内登录账户，完成付款,点击Enter退出购票")
 
+    time.sleep(5)
     driver.close()
     exit(1)
